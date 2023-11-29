1edb17a9047e77e5e0c522ccf575ed51b5689bf9
Muhammad Umaid Khan Khakwani <85894403+umaidkhakwani@users.noreply.github.com>
2023-11-29 17:19:12 +0500

    Update db.js

--- backend/Database/db.js
+++ backend/Database/db.js
@@ -1,9 +1,9 @@
 const mysql = require('mysql2/promise');
 
 const connection = mysql.createPool({
-  host: 'localhost',
+  host: '127.0.0.1',
   user: 'root',
-  password: '',
+  password: '12345678',
   database: 'mep_store',
   connectionLimit: 10, // You can adjust the connection limit as needed
   timezone: 'Z',
